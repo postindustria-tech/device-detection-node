4c976aa9ed97039eb707688f028172547f857aeb
romansertsov <roman.sertsov@postindustria.com>
2023-06-02 14:40:30 +0300

    trying another option

--- ci/run-unit-tests.ps1
+++ ci/run-unit-tests.ps1
@@ -3,6 +3,14 @@ param (
     [string]$RepoName
 )
 
+$env:RESOURCE_KEY = $Options.Keys.TestResourceKey
+$env:TEST_SUPER_RESOURCE_KEY = $Options.Keys.TestSuperResourceKey
+$env:TEST_PLATFORM_RESOURCE_KEY = $Options.Keys.TestPlatformResourceKey
+$env:TEST_HARDWARE_RESOURCE_KEY = $Options.Keys.TestHardwareResourceKey
+$env:TEST_BROWSER_RESOURCE_KEY = $Options.Keys.TestBrowserResourceKey
+$env:TEST_NO_SETHEADER_RESOURCE_KEY = $Options.Keys.TestNoSetHeaderResourceKey
+$env:TEST_LICENSE_KEY = $Options.Keys.TestLicenseKey
+
 ./node/run-unit-tests.ps1 -RepoName $RepoName
 
 exit $LASTEXITCODE

60b61502f18ed513a269c87e1a039aa4a0b2d1d8
romansertsov <roman.sertsov@postindustria.com>
2023-06-07 23:08:35 +0300

    logger

--- ci/build-package.ps1
+++ ci/build-package.ps1
@@ -11,4 +11,5 @@ $noRemote = ""
 
 ./node/build-package-npm.ps1 -RepoName $RepoName -Packages $packages -NoRemote $noRemote -Version $Version
 
+
 exit $LASTEXITCODE
--- ci/run-integration-tests.ps1
+++ ci/run-integration-tests.ps1
@@ -3,6 +3,9 @@ param (
     [string]$RepoName
 )
 
+
+./build-extension.ps1 -PackageName $RepoName
+
 ./node/run-integration-tests.ps1 -RepoName $RepoName
 
 exit $LASTEXITCODE

9a1a25c870b13ee999e5d5d09feb13e2e6dc00d3
romansertsov <roman.sertsov@postindustria.com>
2023-06-08 11:24:21 +0300

    env setup

--- ci/setup-environment.ps1
+++ ci/setup-environment.ps1
@@ -78,6 +78,15 @@ Write-Output "Package configuration file created successfully."
 
 Pop-Location
 
+$env:RESOURCE_KEY = $Options.Keys.TestResourceKey
+$env:TEST_SUPER_RESOURCE_KEY = $Options.Keys.TestSuperResourceKey
+$env:TEST_PLATFORM_RESOURCE_KEY = $Options.Keys.TestPlatformResourceKey
+$env:TEST_HARDWARE_RESOURCE_KEY = $Options.Keys.TestHardwareResourceKey
+$env:TEST_BROWSER_RESOURCE_KEY = $Options.Keys.TestBrowserResourceKey
+$env:TEST_NO_SETHEADER_RESOURCE_KEY = $Options.Keys.TestNoSetHeaderResourceKey
+$env:TEST_LICENSE_KEY = $Options.Keys.TestLicenseKey
+
+
 ./node/setup-environment.ps1 -RepoName $RepoName
 
 if ($LASTEXITCODE -ne 0) {

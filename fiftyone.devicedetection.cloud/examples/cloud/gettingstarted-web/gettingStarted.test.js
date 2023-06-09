b00f2affd9de0b00eae2402e380f657a068ddaed
romansertsov <roman.sertsov@postindustria.com>
2023-06-09 09:16:31 +0300

    logging options

--- fiftyone.devicedetection.cloud/examples/cloud/gettingstarted-web/gettingStarted.test.js
+++ fiftyone.devicedetection.cloud/examples/cloud/gettingstarted-web/gettingStarted.test.js
@@ -39,6 +39,8 @@ describe('Examples', () => {
     // Load configuration options
     const options = JSON.parse(fs.readFileSync(path.join(__dirname, '/51d.json')));
 
+    console.log(options)
+
     // Update element path with a full path
     OptionsExtension.updateElementPath(options, __dirname);
     OptionsExtension.setResourceKey(

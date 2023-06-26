7cd7b8d86bafa8e9cb16c66ae4b03ff9610e3ceb
romansertsov <roman.sertsov@postindustria.com>
2023-06-27 01:34:20 +0300

    pathing

--- ci/run-performance-tests.ps1
+++ ci/run-performance-tests.ps1
@@ -11,7 +11,7 @@ try
     Write-Output "Running performance tests"
     $env:JEST_JUNIT_OUTPUT_DIR = 'test-results/performance'
 
-    $perfSummary = New-Item -ItemType directory -Path $RepoName/test-results/performance-summary -Force
+    $perfSummary = New-Item -ItemType directory -Path test-results/performance-summary -Force
     $perfJSONOutputName = "results_$Name.json";
 
     node fiftyone.devicedetection.onpremise/examples/onpremise/performance-console/performance.js --jsonoutput $perfJSONOutputName || $($testsFailed = $true)

f00c46d78c5e7d11faa91b7498705d0fdf23ac58
romansertsov <roman.sertsov@postindustria.com>
2023-07-25 18:17:09 +0300

    build package requirements added

--- ci/build-package-requirements.ps1
+++ ci/build-package-requirements.ps1
@@ -1,1 +1,51 @@
-Write-Output "No requirements needed"
+param (
+    [Parameter(Mandatory=$true)]
+    [string]$RepoName
+)
+
+Write-Output "Building extension for $package"
+
+# Installing binary builder
+npm install node-gyp --global || $(throw "ERROR: Failed to install node-gyp")
+
+# Determine the operating system
+if ($IsMacOS) {
+    $os = "darwin"
+} elseif ($IsLinux) {
+    $os = "linux"
+sudo apt-get install g++ make libatomic1
+} elseif ($IsWindows) {
+    $os = "win32"
+} else {
+    $os = "unknown"
+}
+
+# Creating folder for future build
+New-Item -ItemType Directory -Path build | Out-Null
+
+# Renaming buiding config file
+Rename-Item -Path binding.51d -NewName binding.gyp
+
+# Run configuration
+node-gyp configure || $(throw "ERROR: Failed to configure node-gyp")
+
+# Build configuration
+node-gyp build || $(throw "ERROR: Failed build with node-gyp")
+
+# Move build result from release folder to lower level (build folder)
+Move-Item -Path "$RepoName/fiftyone.devicedetection.onpremise/build/Release/FiftyOneDeviceDetectionHashV4.node" -Destination "./build/"
+
+# Getting major node version
+$nodeVersion = node --version  || $(throw "ERROR: Failed to get node version")
+$nodeMajorVersion = $nodeVersion.TrimStart('v').Split('.')[0]
+
+
+# Renaming building config file
+$fileName = "FiftyOneDeviceDetectionHashV4-$os-$nodeMajorVersion.node"
+Rename-Item -Path "$RepoName/fiftyone.devicedetection.onpremise/build/FiftyOneDeviceDetectionHashV4.node" -NewName $fileName
+
+# Storing binary artifact
+Copy-Item -Path "$RepoName/fiftyone.devicedetection.onpremise/build/$fileName" -Destination "package-files/"
+
+# Installing package for some examples
+npm install n-readlines || $(throw "ERROR: Failed to install n-readlines")

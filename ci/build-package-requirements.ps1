463e4951ed211ac8f49afbce294455ab01e3e2be
romansertsov <roman.sertsov@postindustria.com>
2023-07-25 23:35:19 +0300

    clean

--- ci/build-package-requirements.ps1
+++ ci/build-package-requirements.ps1
@@ -45,26 +45,11 @@ $fileName = "FiftyOneDeviceDetectionHashV4-$os-$nodeMajorVersion.node"
 
 Rename-Item -Path "./build/Release/FiftyOneDeviceDetectionHashV4.node" -NewName $fileName
 
-# Replace "destination_folder\" with the path of the destination folder.
-$destinationFolder = "C:\path\to\destination_folder\"
-
-# Check if the destination folder exists; if not, create it.
-if (-Not (Test-Path "../../package-files" -PathType Container)) {
-    New-Item -ItemType Directory -Path "../../package-files" | Out-Null
-}
+# Creating folder for binaries artifacts
+New-Item -ItemType Directory -Path "../../package-files" | Out-Null
 
 # Move build result from release folder to lower level (build folder)
 Move-Item -Path "./build/Release/$fileName" -Destination "../../package-files"
-
-$items = Get-ChildItem -Path "../../package-files"
-
-# Use the $items variable as needed in your script.
-# For example, you can iterate through the items, log them to a file, or perform additional actions.
-foreach ($item in $items) {
-    Write-Host "LOGGER:"
-    Write-Host $item.FullName
-}
-
 
 # Installing package for some examples
 npm install n-readlines || $(throw "ERROR: Failed to install n-readlines")

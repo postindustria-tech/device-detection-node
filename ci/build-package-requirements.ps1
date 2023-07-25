35cec15d50f8f01c0683fe24b216b9e1fae79184
romansertsov <roman.sertsov@postindustria.com>
2023-07-25 23:06:33 +0300

    idk

--- ci/build-package-requirements.ps1
+++ ci/build-package-requirements.ps1
@@ -36,22 +36,15 @@ node-gyp configure || $(throw "ERROR: Failed to configure node-gyp")
 # Build configuration
 node-gyp build || $(throw "ERROR: Failed build with node-gyp")
 
-# Move build result from release folder to lower level (build folder)
-Move-Item -Path "./build/Release/FiftyOneDeviceDetectionHashV4.node" -Destination "./build/"
-
 # Getting major node version
 $nodeVersion = node --version  || $(throw "ERROR: Failed to get node version")
 $nodeMajorVersion = $nodeVersion.TrimStart('v').Split('.')[0]
 
-
 # Renaming building config file
-$fileName = "FiftyOneDeviceDetectionHashV4-$os-$nodeMajorVersion.node"
-Rename-Item -Path "./build/FiftyOneDeviceDetectionHashV4.node" -NewName $fileName
-
-
-
-# Storing binary artifact
-Copy-Item -Path "./build/$fileName" -Destination "./package-files/"
+$fileName = "./build/Release/FiftyOneDeviceDetectionHashV4-$os-$nodeMajorVersion.node"
+Rename-Item -Path "./build/Release/FiftyOneDeviceDetectionHashV4.node" -NewName $fileName
+# Move build result from release folder to lower level (build folder)
+Move-Item -Path "./build/Release/$fileName" -Destination "../../package-files/"
 
 $items = Get-ChildItem -Path "./package-files"
 

34ffaa86704ff6d27d9cc6bd3227b5b98f1433b8
romansertsov <roman.sertsov@postindustria.com>
2023-07-26 14:11:03 +0300

    loggers cleanup

--- ci/build-package.ps1
+++ ci/build-package.ps1
@@ -17,16 +17,6 @@ New-Item -ItemType Directory -Path "$RepoName/fiftyone.devicedetection.onpremise
 # Copying all binaries to package build folder
 Copy-Item -Path "./package-files/*/*.node" -Destination "$RepoName/fiftyone.devicedetection.onpremise/build" -Recurse
 
-# Store the output in a variable for later use.
-$items = Get-ChildItem -Path "./package-files"
-
-# Use the $items variable as needed in your script.
-# For example, you can iterate through the items, log them to a file, or perform additional actions.
-foreach ($item in $items) {
-    Write-Output "LOGGER:"
-    Write-Host $item.FullName
-}
-
 ./node/build-package-npm.ps1 -RepoName $RepoName -Packages $packages -NoRemote $noRemote -Version $Version
 
 

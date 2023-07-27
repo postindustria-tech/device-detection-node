1adc53e9642edecc9ba2d64c8e7c21dcbeaec9fc
romansertsov <roman.sertsov@postindustria.com>
2023-07-27 10:33:17 +0300

    pre build step changed

--- ci/build-package-requirements.ps1
+++ ci/build-package-requirements.ps1
@@ -3,55 +3,9 @@ param (
     [string]$RepoName
 )
 
-Push-Location "$RepoName/fiftyone.devicedetection.onpremise";
+$packageWithExtenssions = "fiftyone.devicedetection.onpremise";
 
-Write-Output "Building extension for $package"
+Push-Location $RepoName
+Push-Location $packageWithExtenssions
 
-# Installing binary builder
-npm install node-gyp --global || $(throw "ERROR: Failed to install node-gyp")
-
-# Determine the operating system
-if ($IsMacOS) {
-    $os = "darwin"
-} elseif ($IsLinux) {
-    $os = "linux"
-sudo apt-get install g++ make libatomic1
-} elseif ($IsWindows) {
-    $os = "win32"
-} else {
-    $os = "unknown"
-}
-
-
-# Creating folder for future build
-New-Item -ItemType Directory -Path build | Out-Null
-
-# Renaming buiding config file
-Rename-Item -Path binding.51d -NewName binding.gyp
-
-
-# Run configuration
-node-gyp configure || $(throw "ERROR: Failed to configure node-gyp")
-
-# Build configuration
-node-gyp build || $(throw "ERROR: Failed build with node-gyp")
-
-# Getting major node version
-$nodeVersion = node --version  || $(throw "ERROR: Failed to get node version")
-$nodeMajorVersion = $nodeVersion.TrimStart('v').Split('.')[0]
-
-# Renaming building config file
-$fileName = "FiftyOneDeviceDetectionHashV4-$os-$nodeMajorVersion.node"
-
-Rename-Item -Path "./build/Release/FiftyOneDeviceDetectionHashV4.node" -NewName $fileName
-
-# Creating folder for binaries artifacts
-New-Item -ItemType Directory -Path "../../package-files" | Out-Null
-
-# Move build result from release folder to lower level (build folder)
-Move-Item -Path "./build/Release/$fileName" -Destination "../../package-files"
-
-# Installing package for some examples
-npm install n-readlines || $(throw "ERROR: Failed to install n-readlines")
-
-Pop-Location
+./node/build-extension.ps1 -PackageName $packageWithExtenssions
